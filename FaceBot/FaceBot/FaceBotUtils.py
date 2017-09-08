9045d669d9ad687c1f996659d869308d4a511c5a
David Flavigné <david.flavigne@coding-academy.fr>
2017-09-08 19:25:31 +0200

    Simplified router class

--- FaceBot/FaceBot/FaceBotUtils.py
+++ FaceBot/FaceBot/FaceBotUtils.py
@@ -5,7 +5,6 @@ import shlex
 
 class Router:
     """ Class Router : redirects user input to corresponding callbacks, if any """
-    _routes = {}
     @staticmethod
     def route(bot,command):
         """ 
@@ -15,26 +14,40 @@ class Router:
         """
         args = shlex.split(command)
         if args[0] == 'message':
-            if len(args) < 3:
-                print('Usage : message <friendname> <message body>')
-                return False
-            return bot.message(args[1],args[2])
+            return Router.message(args[1],args[2])
         elif args[0] == 'notifications':
-            notifs = bot.get_notifications()
-            if not notifs is False:
-                for notif in notifs:
-                    print('        '+notif)
+            return Router.notifications(bot)
+        elif args[0] == 'list' or args[0] == 'help':
+            return Router.usage()
+        else:
+            print('Bad command name: type "list" to get full commands list')
+            return False
+        
+    @staticmethod
+    def messager(friend, message):
+        if len(args) < 3:
+            print('Usage : message <friendname> <message body>')
+            return False
+        return bot.message(friend,message)
+        
+    @staticmethod
+    def usage():
+        print('Commands list:')
+        print('    notifications: print last notifications')
+        print('    message: send a message to a friend')
+        print('    list: print this list')
+        print('    stop: stop the bot and logout')
+        return True
+    
+    @staticmethod
+    def notifications(bot):
+        notifs = bot.get_notifications()
+        if not notifs is False:
+            for notif in notifs:
+                print('        '+notif)
                 return True
             else:
                 print('No notifications found')
                 return False
-        elif args[0] == 'list' or args[0] == 'help':
-            print('Commands list:')
-            print('    notifications: print last notifications')
-            print('    message: send a message to a friend')
-            print('    list: print this list')
-            print('    stop: stop the bot and logout')
-            return True
-        else:
-            print('Bad command name: type "list" to get full commands list')
-            return False
+            
+        
